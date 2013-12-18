8248dd109030e93460a0715d9ce750d1ff47b2c1
Mauricio Ruiz <maudiruiz@hotmail.com>
2013-12-17 18:38:48 -0600

    Todo el login basodo el login basico funcionando chingon

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :name, :gender, :birthday
+  # attr_accessible :title, :body
+
+	  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+	  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+	  unless user
+	  	logger.info auth
+	    user = User.create(name:auth.extra.raw_info.name,
+	                         provider:auth.provider,
+	                         uid:auth.uid,
+	                         gender:auth.extra.raw_info.gender,
+	                         birthday:auth.extra.raw_info.birthday,
+	                         email:auth.info.email,
+	                         password:Devise.friendly_token[0,20]
+	                         )
+	  end
+	  user
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elysa::Application.config.secret_token = '99ab046ab4e4aeea7899991bf70c0da91960d1145d9cf77aabf7cd73ba62e6c349a3a34e5654168118a6ae068ebe7b86de68eb31ad5ab5796de7e6589dba50e7'
